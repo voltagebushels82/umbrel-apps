9b61db0bfd37c472232718769ed56fcd7fcf27f8
Voltage Bushels <voltage_bushels82@icloud.com>
2025-02-06 22:28:56 +0000

    expose APP_RESTRICTED_RPC_PORT port for monerod

--- monero/exports.sh
+++ monero/exports.sh
@@ -79,8 +79,6 @@ fi
 export MONERO_DEFAULT_NETWORK="${MONERO_CHAIN}"
 
 BIN_ARGS=()
-BIN_ARGS+=( "--rpc-bind-port=${APP_MONERO_RPC_PORT}" )
-BIN_ARGS+=( "--rpc-bind-ip=0.0.0.0" )
 BIN_ARGS+=( "--confirm-external-bind" )
 
 BIN_ARGS+=( "--rpc-login=\"${APP_MONERO_RPC_AUTH}\"" )
